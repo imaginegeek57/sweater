33ea56f57f120d9cb67758af91a85af9e2269358
imaginegeek57 <imaginegeek57@gmail.com>
2020-10-31 16:21:49 +0300

    add freemarker

--- src/main/java/com/sweater/controller/MainController.java
+++ src/main/java/com/sweater/controller/MainController.java
@@ -1,9 +1,12 @@
 package com.sweater.controller;
 
 import com.sweater.domain.Message;
+import com.sweater.domain.User;
 import com.sweater.repositories.MessageRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,7 +17,7 @@ import java.util.Map;
 public class MainController {
 
     @Autowired
-    private MessageRepository messageRepository;
+    private MessageRepository messageRepo;
 
     @GetMapping("/")
     public String greeting(Map <String, Object> model) {
@@ -22,33 +25,35 @@ public class MainController {
     }
 
     @GetMapping("/main")
-    public String main(Map <String, Object> model) {
-        Iterable <Message> messages = messageRepository.findAll();
+    public String main(@RequestParam(required = false, defaultValue = "") String filter, Model model) {
+        Iterable <Message> messages = messageRepo.findAll();
 
-        model.put("messages", messages);
+        if (filter != null && !filter.isEmpty()) {
+            messages = messageRepo.findByTag(filter);
+        } else {
+            messages = messageRepo.findAll();
+        }
+
+        model.addAttribute("messages", messages);
+        model.addAttribute("filter", filter);
+
         return "main";
     }
 
     @PostMapping("/main")
-    public String add(@RequestParam String text, @RequestParam String tag, Map <String, Object> model) {
-        Message message = new Message(text, tag);
-        messageRepository.save(message);
-        Iterable <Message> messages = messageRepository.findAll();
+    public String add(
+            @AuthenticationPrincipal User user,
+            @RequestParam String text,
+            @RequestParam String tag, Map <String, Object> model
+    ) {
+        Message message = new Message(text, tag, user);
+
+        messageRepo.save(message);
+
+        Iterable <Message> messages = messageRepo.findAll();
 
         model.put("messages", messages);
-        return "main";
-    }
 
-    @PostMapping("filter")
-    public String filter(@RequestParam String filter, Map <String, Object> model) {
-        Iterable <Message> messages;
-        if (filter != null && !filter.isEmpty()) {
-            messages = messageRepository.findByTag(filter);
-        } else {
-            messages = messageRepository.findAll();
-        }
-        model.put("messages", messages);
         return "main";
     }
 }
-
--- src/main/java/com/sweater/domain/Role.java
+++ src/main/java/com/sweater/domain/Role.java
@@ -1,5 +1,12 @@
 package com.sweater.domain;
 
-public enum Role {
+import org.springframework.security.core.GrantedAuthority;
+
+public enum Role implements GrantedAuthority {
     USER;
+
+    @Override
+    public String getAuthority() {
+        return name();
+    }
 }
